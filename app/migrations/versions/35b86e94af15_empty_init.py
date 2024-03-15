4805f2b224b0719430a9cbe3cd0490348b2a6f08
Robins <robinmgk077@gmail.com>
2024-03-15 22:45:32 +0300

    Completed Assignment

---
+++ app/migrations/versions/35b86e94af15_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 35b86e94af15
+Revises: 
+Create Date: 2024-03-15 22:38:50.991286
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '35b86e94af15'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
